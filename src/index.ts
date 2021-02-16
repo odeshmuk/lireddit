f28ded944e0cbed31ce71f1d802813b184e83977
Omkar Deshmukh <omdesh.0407@gmail.com>
2021-02-16 16:46:10 +0530

    Adding mikro-orm Create/Read

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
+import {MikroORM } from "@mikro-orm/core"
+import { __prod__ } from "./constants";
+import { Post } from "./entities/post";
+import mikroConfig from './mikro-orm.config';
+
+const main = async () => {
+    const orm=await MikroORM.init(mikroConfig);  
+    await orm.getMigrator().up();
+    // const post=orm.em.create(Post,{title:'My first Post'})
+    // await orm.em.persistAndFlush(post);
+
+    const posts= await orm.em.find(Post,{})
+    console.log(posts);
+}
+main().catch((err)=>{
+    console.log(err);
+});
+
+
+
 console.log('hello world')
